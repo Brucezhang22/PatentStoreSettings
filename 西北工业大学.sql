08f148878778ae0b1a0a71314df40702d03b2fd8
MudOnTire <895157882@qq.com>
2018-04-20 10:21:57 +0800

    西北工业大学

---
+++ 西北工业大学.sql
@@ -0,0 +1,7 @@
+INSERT INTO `iptp1v`.`sys_account` (`prod_type`, `username`, `password`, `salt`, `name`, `status`, `create_time`, `platform_id`, `sex`) VALUES ('2', 'xbgydx', 'uIss8e4XU1zphoesnfarNwzUrlE=', 'SFez5kL0MZg=', '西北工业大学', '1', '2018-04-20 09:13:03', '20', '3');
+
+
+INSERT INTO `sys_user` (`account_id`, `user_type`, `college_id`, `create_time`, `update_time`, `platform_id`, `login_times`) VALUES ('616', '2', '17', '2018-04-20 09:30:00', '2018-04-20 09:33:00', '20', '0');
+
+
+INSERT INTO `sys_account_role` (`platform_id`, `account_id`, `role_id`) VALUES ('20', '616', '201');
